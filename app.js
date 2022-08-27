350e56a289897758674060544ee940dad4c9dc91
Sebastian <sebasgc0399@gmail.com>
2022-08-27 16:29:46 -0500

    Primer Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+console.log('Peticion Recibida')
+
+res.send('Hola mundo')
+});
+
+app.listen(4000, () => {
+    console.log('Servidor escuchando en el puerto 4000')
+})
